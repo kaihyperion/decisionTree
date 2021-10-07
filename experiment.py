e5cdd70d7e0091a90c863b5c7ade4e99055e82c1
kaihyperion <77666174+kaihyperion@users.noreply.github.com>
2021-10-08 04:18:27 +0900

    Update experiment.py

--- experiment.py
+++ experiment.py
@@ -29,7 +29,31 @@ def run(data_path, learner_type, fraction):
         f1_measure (np.float): F1 Measure on testing examples using learner
     """
 
-    raise NotImplementedError()
+    #1 Take in a path to dataset and use load_data from .data.py
+
+    features, targets, attribute_names = load_data(data_path)
+
+    #2 Initiates the class used for learning from the data using learner_type
+    # (.e.g learner_type is decision_tree', 'prior_probability')
+
+    if fraction == 1.0:
+        training_features = testing_features = features
+        training_targets = testing_targets = targets
+    else:
+        training_features, training_targets, testing_features, testing_targets \
+            = train_test_split(features, targets, fraction)
+
+    if learner_type == "prior_probability":
+        PriorProbability().fit(training_features, training_targets)
+        prediction = PriorProbability().predict(testing_features)
+    else:
+        DecisionTree(attribute_names).fit(training_features, training_targets)
+        prediction = DecisionTree(attribute_names).predict(testing_features)
+
+    confuscious_matrix = confusion_matrix(testing_targets, prediction)
+    accuraC = accuracy(testing_targets, prediction)
+    precision, recall = precision_and_recall(testing_targets, prediction)
+    formula_1 = f1_measure(testing_targets, prediction)
 
     # Order of these returns must be maintained
-    return confusion_matrix, accuracy, precision, recall, f1_measure
+    return confuscious_matrix, accuraC, precision, recall, formula_1
